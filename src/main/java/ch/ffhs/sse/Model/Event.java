83fb0881e90a68b4eed5014d8220af1755220e45
steve <steven.meyer@students.ffhs.ch>
2022-10-22 14:30:31 +0200

    updating Event Model updating EventRepository

--- src/main/java/ch/ffhs/sse/Model/Event.java
+++ src/main/java/ch/ffhs/sse/Model/Event.java
@@ -9,35 +9,78 @@ import java.sql.Timestamp;
 @Entity
 public class Event implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private long eventId;
 
     @Enumerated(EnumType.ORDINAL)
     private EventType eventType;
 
     @Column
-    @NotNull
+    //@NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm a z")
     //@JsonFormat(pattern="dd/MM/yyyy hh:mm")
     private Timestamp start;
 
     @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm a z")
     private Timestamp end;
 
     @Column
     private boolean allDay;
 
-    @ManyToOne//(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne //(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name="userId",nullable = false)
     private User user;
+
+    public Event(long eventId, boolean allDay, Timestamp start, Timestamp end, EventType eventType, User user {
+        this.eventId = eventId;
+        this.allDay = allDay;
+        this.start = start;
+        this.end = end;
+        this.eventType = eventType;
+        this.user = user.getUserId();
+    }
 
     public Event(){
 
 
     }
+    public long getEventId() {
+        return eventId;
+    }
 
-    //@OneToOne(mappedBy = "event")
-    //@JoinColumn(name ="eventId", insertable=false, updatable=false)
-    //private User user;
+    public void setEventId(long id) {
+        this.eventId = id;
+    }
+    //public long getUserId() {
+    //    return getUserId();
+    //}
+
+    public void setUserId(long id) {
+        setUserId(id);
+    }
+        public Timestamp getEnd() {
+            return end;
+
+    }
+    public void setEnd(Timestamp end) {
+        this.end = end;
+    }
+    public boolean getAllDay() {
+        return allDay;
+
+    }
+    public Timestamp getStart() {
+        return start;
+
+    }
+    public void setStart(Timestamp start) {
+        this.start = start;
+    }
+    public void setAllDay(boolean allDay) {
+        this.allDay = allDay;
+    }
+
+
 }
