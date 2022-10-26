c1b50d27842fb5fad1b89cc3e5117f0b9049ffe3
Ennis <66329667+ennisaliu@users.noreply.github.com>
2022-10-26 18:44:39 +0200

    added minor exception Handling in EventController

--- src/main/java/ch/ffhs/sse/Controller/EventController.java
+++ src/main/java/ch/ffhs/sse/Controller/EventController.java
@@ -1,5 +1,6 @@
 package ch.ffhs.sse.Controller;
 
+import ch.ffhs.sse.Exception.UserNotFound;
 import ch.ffhs.sse.Model.Event;
 import ch.ffhs.sse.Model.User;
 import ch.ffhs.sse.Repository.EventRepository;
@@ -33,18 +34,24 @@ public class EventController {
     // In order to save an event with the user we need to instantiate both objects.
     // We then find both objects by id and assign the user to event.
     @PutMapping("/{eventId}/users/{userId}")
-    Event addUserToEvent (
+    String addUserToEvent (
             @PathVariable Long eventId,
             @PathVariable Long userId
     ) {
         try {
             // Get the event and user by ID
+
             Event event = eventRepository.findById(eventId).get();
             User user = userRepository.findById(userId).get();
-            // Add user object to the event object and save it by using the save method from event repo..
-            event.getEventParticipants().add(user);
-            // Return the saved event
-            return eventRepository.save(event);
+            if (!user.equals(null) || !event.equals(null)) {
+                // Add user object to the event object and save it by using the save method from event repo..
+                event.getEventParticipants().add(user);
+                // Return the saved event
+                return user.getFirstName() +" " + user.getLastName() + "was added to the Event ID:  " + event.getEventId();
+            } else {
+                throw new Exception("User or Event not found");
+            }
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
